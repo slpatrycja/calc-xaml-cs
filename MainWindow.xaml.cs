b47657d60a6f661fca4697294be73b4ceacb61fb
slpatrycja <sl.patrycja.of@gmail.com>
2018-06-12 13:37:19 +0200

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kalkulator
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            string id = btn.Uid.ToString();
+
+            input.Text += id;   
+        }
+
+        static Double Eval(String expression)
+        {
+            System.Data.DataTable table = new System.Data.DataTable();
+            return Convert.ToDouble(table.Compute(expression, String.Empty));
+        }
+
+        private void Count(object sender, RoutedEventArgs e)
+        {
+
+            var result = Eval(input.Text);
+            input.Text = result.ToString();
+          
+        }
+    }
+}
